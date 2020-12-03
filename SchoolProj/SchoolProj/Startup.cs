98851a741704a71c993e4c395908fddedaf79a56
Kirill <kirillaromad@mail.ru>
2020-12-04 00:27:21 +0300

    main page

--- SchoolProj/SchoolProj/Startup.cs
+++ SchoolProj/SchoolProj/Startup.cs
@@ -24,11 +24,15 @@ namespace SchoolProj
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddRazorPages();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+            app.UseSession();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
