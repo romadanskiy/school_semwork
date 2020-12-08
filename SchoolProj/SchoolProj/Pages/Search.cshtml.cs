c5b042cf35217d57d47085a79a0e825c60b22a0f
Kirill <kirillaromad@mail.ru>
2020-12-08 20:30:17 +0300

    add search page

---
+++ SchoolProj/SchoolProj/Pages/Search.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolProj.Models;
+
+namespace SchoolProj.Pages
+{
+    public class Search : PageModel
+    {
+        public List<Course> Courses;
+        
+        public void OnGet()
+        {
+            var courseDao = new CourseDao();
+            Courses = courseDao.GetAllWithSubjects();
+        }
+    }
+}
