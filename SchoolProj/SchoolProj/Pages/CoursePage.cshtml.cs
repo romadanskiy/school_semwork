2aeffb6b7bd027ccd7f3e67eb35a993f4d5e0245
Kirill <kirillaromad@mail.ru>
2020-12-08 22:41:51 +0300

    fix CoursePage

--- SchoolProj/SchoolProj/Pages/CoursePage.cshtml.cs
+++ SchoolProj/SchoolProj/Pages/CoursePage.cshtml.cs
@@ -7,8 +7,6 @@ namespace SchoolProj.Pages
     public class CoursePage : PageModel
     {
         public Course Course;
-        public List<string> Subjects;
-        public List<Unit> Units;
         public List<Comment> Comments;
         
         public void OnGet(string courseId)
@@ -16,9 +14,6 @@ namespace SchoolProj.Pages
             var id = int.Parse(courseId);
             var courseDao = new CourseDao();
             Course = courseDao.GetById(id);
-            Subjects = courseDao.GetSubjects(id);
-            var unitDao = new UnitDao();
-            Units = unitDao.GetByCourseId(id);
             var commentDao = new CommentDao();
             Comments = commentDao.GetByCourseId(id);
         }
