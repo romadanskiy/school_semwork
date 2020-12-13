bba21834eb202e78d8d4513d4ece422456afd195
Kirill <kirillaromad@mail.ru>
2020-12-13 12:30:46 +0300

    some fix

---
+++ SchoolProj/SchoolProj/DAO/PurchaseDao.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SchoolProj.Models
+{
+    public class PurchaseDao : IDao<Purchase>
+    {
+        public Purchase GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Purchase> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(Purchase t)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(Purchase t)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
